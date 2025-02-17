f4047533571181e73e2304d4f165834bfc9861ca
Killary912 <mayconbackup14@gmail.com>
2025-02-17 18:03:27 -0300

    Update Lista while quest1.cpp

--- Lista while quest1.cpp
+++ Lista while quest1.cpp
@@ -1,29 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h> // Biblioteca para funções utilitárias
+#include <stdlib.h> 
 
 int main() {
     int numero, soma = 0, cont = 0;
     
-    // Loop while para solicitar números do usuário
+
     while (1) {
-        printf("Digite um número inteiro (0 para sair): ");
+        printf("Digite um nÃºmero inteiro (0 para sair): ");
         scanf("%d", &numero);
         
-        // Se o usuário digitar 0, encerra o loop
         if (numero == 0) {
             break;
         }
         
-        // Adiciona o número à soma
         soma += numero;
-        cont++; // Conta quantos números foram digitados
+        cont++; 
     }
     
-    // Utilizando for para exibir resultados
+
     for (int i = 0; i < 1; i++) {
-        printf("A soma dos números digitados é: %d\n", soma);
-        printf("Quantidade de números digitados (excluindo o 0): %d\n", cont);
-        printf("Os números somados resultam em: %d\n", soma);
+        printf("A soma dos nÃºmeros digitados Ã©: %d\n", soma);
+        printf("Quantidade de nÃºmeros digitados (excluindo o 0): %d\n", cont);
+        printf("Os nÃºmeros somados resultam em: %d\n", soma);
     }
     
     return 0;
